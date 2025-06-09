4087d3ea9489e8f254fb8c2bce6c8b944746f37e
Marianna Martins <157758786+martmarianna@users.noreply.github.com>
2025-06-09 16:03:29 -0300

    Add files via upload
    Começo das primeiras funções e esboço do sistema completo. Das 11 as 16, com pausa para almoço.

---
+++ funcao_5.py
@@ -0,0 +1,12 @@
+def quantidade_total_estoque(f):
+    
+    for f in estoque_produtos.values():
+        f += f 
+        return print (f'O total de produtos no estoque é de {total_estoque}.')
+
+    #início do algoritmo
+
+estoque_produtos = {'Macarrão': [8.99, 10.20, 10]}
+total_estoque = 0
+
+quantidade_total_estoque(total_estoque)
